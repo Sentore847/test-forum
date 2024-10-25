d254e8f6aa629e16d16e6fd2f401923c639d0e1e
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-25 12:49:15 +0300

    add options

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,14 @@
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export function buildPlugins({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
+}
