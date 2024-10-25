e934ce0c4260734bd9c76498dcb8a10bec70ea18
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-25 15:01:42 +0300

    create test app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>React 18 TypeScript App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
