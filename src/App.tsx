456b30fb18fa09f0586e24cc659e97dbec7423ff
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-27 18:49:29 +0200

    Create themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 import React, { Suspense } from "react";
-import "./index.css";
+import "./styles/index.scss";
 
 const App: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <span onClick={toggleTheme}>
+        {theme === Theme.DARK ? <MdLightMode /> : <MdDarkMode />}
+      </span>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
