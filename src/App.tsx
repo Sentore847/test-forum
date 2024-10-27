6873d00a3631344b4dd24f1c620fd35b66720ee2
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-27 11:35:23 +0200

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import Counter from "./components/Counter";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import React, { Suspense } from "react";
 import "./index.css";
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
