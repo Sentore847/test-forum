238dcf215151ddefc4720c11a88073751c752c46
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-27 18:47:57 +0200

    Change logic for styles

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,27 +1,18 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const cssLoader = {
-    test: /\.css$/i,
-    use: [
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      "css-loader",
-    ],
-  };
-
-  const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       {
         loader: "css-loader",
         options: {
           modules: {
-            localIdentName: options.isDev
-              ? "[name]__[local]"
-              : "[hash:base64:5]",
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
           },
         },
       },
@@ -35,5 +26,5 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader, scssLoader];
+  return [typescriptLoader, cssLoader];
 }
