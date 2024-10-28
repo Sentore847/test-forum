a7476e216156ef6f7beb29f57500a509371e4ae0
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-28 11:58:22 +0200

    Moved theme button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,3 @@
-import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { Theme } from "./providers/ThemeProvider/lib/ThemeContext";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
@@ -7,14 +5,11 @@ import { Navbar } from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App: React.FC = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <span onClick={toggleTheme}>
-        {theme === Theme.DARK ? <MdLightMode /> : <MdDarkMode />}
-      </span>
     </div>
   );
 };
