11c1b95bfb7f4cfb986c948b9cc30cccae070a13
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-28 10:43:31 +0200

    Deleted links from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,20 @@
-import { Link } from "react-router-dom";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { Theme } from "./providers/ThemeProvider/lib/ThemeContext";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <span onClick={toggleTheme}>
         {theme === Theme.DARK ? <MdLightMode /> : <MdDarkMode />}
       </span>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
-      <AppRouter />
     </div>
   );
 };
