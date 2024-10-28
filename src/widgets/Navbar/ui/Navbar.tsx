8ca845259db8cdaff3c8abd157648fdc429543cb
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-28 17:24:14 +0200

    Added lang switch component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import { ThemeSwitch } from "widgets/ThemeSwitch";
 import cls from "./Navbar.module.scss";
+import { LangSwitch } from "widgets/LangSwitch/LangSwitch";
 
 interface NavbarProps {
   className?: string;
@@ -11,6 +12,7 @@ export function Navbar({ className }: NavbarProps) {
   return (
     <div className={classNames(cls.navbar, {}, [])}>
       <ThemeSwitch />
+      <LangSwitch className={cls.lang} />
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
