2d98fb0bcf03b72e195cd3ae1ce86f5dbe8dfd67
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-28 17:23:01 +0200

    Added suspense for loading translate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,20 @@ import { classNames } from "../shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
 import "./styles/index.scss";
 
 const App: React.FC = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback={<div>Loading translations...</div>}>
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
