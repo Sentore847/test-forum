a24127fd3294d43945bd4591f5aa69557188b0a5
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-28 12:01:42 +0200

    Created button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { ButtonHTMLAttributes, FC, ReactNode } from "react";
+import cls from "./Button.module.scss";
+
+export enum ThemeButton {
+  CLEAR = "clear",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  children?: ReactNode;
+  theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { className, theme, children, ...otherProps } = props;
+  return (
+    <button
+      className={classNames(cls.navbar, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
