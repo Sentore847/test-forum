7324a20b218620219768d786578db3084932e3c3
Sentore847 <grigorchuk.dev@gmail.com>
2024-10-31 23:31:20 +0200

    Refactored

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,18 +2,15 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mods?: Mods,
-  additional?: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
-      .filter(([value]) => Boolean(value))
+    ...Object.entries(mods)
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(" ");
 }
-
-classNames("remove-btn", { hovered: true, selectable: true, red: false }, [
-  "pdg",
-]);
