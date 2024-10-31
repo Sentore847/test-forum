ad67a8993ddd282bc9091691a7d1ab7dc2acaef9
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 01:45:50 +0200

    Added BugButton for test

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,12 @@
+import { BugButton } from "app/providers/ErrorBoundary";
 import { useTranslation } from "react-i18next";
 
 export default function MainPage() {
   const { t } = useTranslation("main");
-  return <div>{String(t("Main Page"))}</div>;
+  return (
+    <div>
+      <BugButton />
+      {String(t("Main Page"))}
+    </div>
+  );
 }
