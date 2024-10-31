6bdb2081ff3de4d7fd17d31f0ebfe76dd16685fe
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 01:45:19 +0200

    Created BugButton component for test

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { Button } from "shared/ui/Button/Button";
+
+// Component for test errors
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+
+  const onThrow = () => setError(true);
+
+  useEffect(() => {
+    if (error) throw new Error();
+  }, [error]);
+  return <Button onClick={onThrow}>throw Error</Button>;
+};
