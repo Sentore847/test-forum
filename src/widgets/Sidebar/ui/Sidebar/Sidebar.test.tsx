61e774354fb37854b1c045b44f3bfea610f5f59c
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 11:35:24 +0200

    Created tests for sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Sidebar } from "./Sidebar";
+
+describe("Sidebar", () => {
+  test("with only first param", () => {
+    render(<Sidebar />);
+    expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+  });
+
+  test("with only first param", () => {
+    render(<Sidebar />);
+    const toggleBtn = screen.getByTestId("sidebar-toggle");
+    expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
+  });
+});
