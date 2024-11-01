d4056ca5b7ed131ea7d6dd336f96051feb949ab3
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-01 11:34:18 +0200

    Added imports css/scss files

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,25 +4,19 @@
  */
 
 export default {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\grigo\\AppData\\Local\\Temp\\jest",
-
-  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
   testEnvironment: "jsdom",
   coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
   moduleDirectories: ["node_modules"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
   testMatch: ["<rootDir>/src/**/*(*.)@(spec|test).[tj]s?(x)"],
-  transformIgnorePatterns: ["/node_modules/"],
+  modulePaths: ["<rootDir>src"],
   rootDir: "../../",
   testPathIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
