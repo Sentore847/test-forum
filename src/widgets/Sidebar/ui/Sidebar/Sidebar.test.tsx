0762d9e7b363d0dfc50941f177219d292c462296
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-05 19:49:47 +0200

    added router

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,24 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom"; // Импортируем BrowserRouter
 import { Sidebar } from "./Sidebar";
 import { act } from "react";
 
 describe("Sidebar", () => {
   test("renders sidebar component", () => {
-    render(<Sidebar />);
+    render(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
+    );
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
 
   test("toggles sidebar on button click", () => {
-    render(<Sidebar />);
+    render(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
+    );
     const toggleBtn = screen.getByTestId("sidebar-toggle");
 
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
