1ae24f5bed6bfcc262a469257717933845915644
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-05 18:59:23 +0200

    Added links, icons

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,10 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { ReactNode, useState } from "react";
-import { Button } from "shared/ui/Button/Button";
+import { Button, ButtonSize, ThemeButton } from "shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import { MdHome } from "react-icons/md";
+import { IoIosListBox } from "react-icons/io";
 import cls from "./Sidebar.module.scss";
 
 interface SidebarProps {
@@ -21,9 +25,35 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
+      <Button
+        className={cls.collapseBtn}
+        theme={ThemeButton.BACKGROUND_INVERTED}
+        data-testid="sidebar-toggle"
+        onClick={onToggle}
+        square
+        size={ButtonSize.L}
+      >
+        {collapsed ? ">" : "<"}
       </Button>
+
+      <div className={cls.items}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.main}
+          className={cls.link}
+        >
+          <MdHome className={cls.sidebarIcon} />
+          {!collapsed && <span>Main</span>}
+        </AppLink>
+        <AppLink
+          className={cls.link}
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.about}
+        >
+          <IoIosListBox className={cls.sidebarIcon} />
+          {!collapsed && <span>About</span>}
+        </AppLink>
+      </div>
     </div>
   );
 };
