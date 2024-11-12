d9e2fe9ae2d676480e70775c779b8b2db550e1c8
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-12 17:47:43 +0200

    added eslint react hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
 export default [
   {
@@ -16,8 +17,13 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "warn",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
