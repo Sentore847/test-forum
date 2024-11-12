c3ea80c8428ddbaed1da84c6ba1b069b76b4c4ad
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-12 17:51:15 +0200

    Added modal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,10 @@
 import { ThemeSwitch } from "widgets/ThemeSwitch";
 import { LangSwitch } from "widgets/LangSwitch/LangSwitch";
 import { classNames } from "shared/lib/classNames/classNames";
+import { Modal } from "shared/ui/Modal/Modal";
+import { useCallback, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -8,8 +12,26 @@ interface NavbarProps {
 }
 
 export function Navbar({ className }: NavbarProps) {
+  const { t } = useTranslation("translation");
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
+      <Button
+        theme={ThemeButton.CLEAR_INVERTED}
+        className={cls.links}
+        onClick={onToggleModal}
+      >
+        {String(t("Login"))}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        {String(t("IS MODAL WINDOW"))}
+      </Modal>
+
       <ThemeSwitch />
       <LangSwitch className={cls.lang} />
     </div>
