cc42e31ed93db96332e234d2682304ca18a4bbb0
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-22 15:16:15 +0200

    Added Counter to MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,12 @@
+import { Counter } from "entities/Counter";
 import { useTranslation } from "react-i18next";
 
 export default function MainPage() {
   const { t } = useTranslation("main");
-  return <div>{String(t("Main Page"))}</div>;
+  return (
+    <div>
+      {String(t("Main Page"))}
+      <Counter />
+    </div>
+  );
 }
