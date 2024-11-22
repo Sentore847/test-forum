89a55acc1db7e6fbaa0248c8d7b38386822c25cf
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-22 15:16:30 +0200

    Created render component for tests

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { ReactNode } from "react";
+import { MemoryRouter } from "react-router-dom";
+import { DeepPartial } from "utility-types";
+
+export interface componentRenderOptions {
+  route?: string;
+  initialState?: DeepPartial<StateSchema>;
+}
+
+export function componentRender(
+  component: ReactNode,
+  options: componentRenderOptions = {}
+) {
+  const { route = "/", initialState } = options;
+
+  return render(
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>{component}</MemoryRouter>
+    </StoreProvider>
+  );
+}
