f4af5a9143ed9e2094d40b1d3e40a68fb024d667
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-25 18:26:57 +0200

    Added input for test

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,18 @@
-import { Counter } from "entities/Counter";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Input } from "shared/ui/Input/Input";
 
 export default function MainPage() {
   const { t } = useTranslation("main");
+  const [value, setValue] = useState("");
+
+  const onChange = (val: string) => {
+    setValue(val);
+  };
   return (
     <div>
       {String(t("Main Page"))}
-      <Counter />
+      <Input placeholder="text..." value={value} onChange={onChange} />
     </div>
   );
 }
