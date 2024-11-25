4c02696ecfc820d09c0a2507e3b67d8680df2e43
Sentore847 <grigorchuk.dev@gmail.com>
2024-11-25 18:41:33 +0200

    Deleted unused

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,8 +1,6 @@
-import { classNames } from "shared/lib/classNames/classNames";
 import { ReactNode } from "react";
 import { Modal } from "shared/ui/Modal/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
-import cls from "./LoginModal.module.scss";
 
 interface LoginModalProps {
   className?: string;
@@ -13,12 +11,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-    <Modal
-      className={classNames(cls.LoginModal, {}, [className])}
-      isOpen={isOpen}
-      onClose={onClose}
-      lazy
-    >
+    <Modal isOpen={isOpen} onClose={onClose} lazy>
       <LoginForm />
     </Modal>
   );
